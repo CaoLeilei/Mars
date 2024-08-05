eab065fe12b84098b055384871d931823f54649d
siyuan.cao <ll900618@163.com>
2024-08-05 19:22:43 +0800

    部分页面以及功能的调整

---
+++ packages/mars-admin/src/views/Workspace/Projects/components/ProjectItemCard.vue
@@ -0,0 +1,51 @@
+<template>
+  <el-card size="small" class="project-card">
+    <div>
+      <h2 class="project-card__title">项目名称</h2>
+      <p class="project-card__desc">项目描述</p>
+    </div>
+
+    <template #footer>
+      <div class="project-card__footer">
+        <div class="project-card__footer-item">
+          <el-button text class="project-card__footer-button">删除</el-button>
+        </div>
+        <el-divider direction="vertical"></el-divider>
+        <div class="project-card__footer-item">
+          <el-button text class="project-card__footer-button">查看</el-button>
+        </div>
+        <el-divider direction="vertical"></el-divider>
+        <div class="project-card__footer-item">
+          <el-button text class="project-card__footer-button">修改</el-button>
+        </div>
+      </div>
+    </template>
+  </el-card>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+.project-card {
+  @apply relative w-full;
+
+  &__title {
+    @apply text-lg font-bold mb-2 line-clamp-1;
+  }
+  &__desc {
+    @apply text-sm text-gray-500 line-clamp-3;
+  }
+
+  &__footer {
+    @apply flex justify-between pt-2 pb-2;
+  }
+  ::v-deep(.ep-card__body) {
+    @apply p-4 leading-6;
+  }
+  ::v-deep(.ep-card__footer) {
+    @apply pt-0 pb-0;
+  }
+}
+</style>
--- packages/mars-admin/src/views/Workspace/Projects/index.vue
+++ packages/mars-admin/src/views/Workspace/Projects/index.vue
@@ -1,21 +1,28 @@
 <template>
   <el-container class="projects">
-    <el-header class="projects__header" height="40px">
-      <el-button type="primary" class="w-12" @click="handleCreateBtnClick">创建项目</el-button>
+    <el-header class="projects__header" height="auto">
+      <el-button type="primary" class="w-12" @click="handleCreateBtnClick">
+        创建项目
+      </el-button>
     </el-header>
     <el-main class="projects__main">
       <el-row :gutter="12">
-        <el-col></el-col>
+        <el-col :span="4">
+          <ProjectItemCard />
+        </el-col>
+        <el-col :span="4">
+          <ProjectItemCard />
+        </el-col>
       </el-row>
     </el-main>
-    <create-project-modal v-model:visible="createModalVisible"></create-project-modal>
+    <create-project-modal v-model:visible="createModalVisible" />
   </el-container>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
 import CreateProjectModal from './components/CreateProjectModal.vue';
-import { create } from 'lodash';
+import ProjectItemCard from './components/ProjectItemCard.vue';
 
 const createModalVisible = ref<boolean>(false)
 
@@ -30,7 +37,7 @@ function handleCreateBtnClick() {
   @apply relative w-full h-full;
 
   &__header {
-    @apply pl-0 pr-0;
+    @apply pl-0 pr-0 pb-4 box-border;
   }
   &__main {
     @apply p-0;
